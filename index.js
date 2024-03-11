e5f547fdba0a6e97150ac826f3b01319e605591c
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-11 16:02:53 +0300

    feat: added functions, ensure running tests

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a+b
+}
+
+function subtract(a, b) {
+  return a-b
+}
+
+function multiply(a, b){
+  return a*b
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+function increment(n){
+  return ++n
+}
+
+function decrement(n){
+  return --n
+}
+
+function makeInt(str){
+  return parseInt(str, 10)
+}
+
+function preserveDecimal(str){
+  return parseFloat(str)
+}
